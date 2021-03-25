4a06f93c97d22f30780eae55aba89a73dfb87bfc
Mary <mari.stagnacheva@gmail.com>
2021-03-25 21:03:04 +0300

    Wanna try css

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,23 @@
-import React, {Component} from 'react'
-import styles from "./Auth.module.css"
+import React, { Component } from 'react';
+import styles from './Auth.module.css';
 
 class Auth extends Component {
-    render() {
-    return(
-        <h1>Auth</h1>
-    )}
+  render() {
+    return (
+      <div className={styles.Auth}>
+        <div>
+        <h1>Авторизация</h1>
+        <form>
+          <label for="Email">Email</label>
+          <input type="text" id="Email"></input>
+          <label for="Пароль">Пароль</label>
+          <input type="text" id="Пароль"></input>
+          <button>Войти</button>
+          <button>Зарегестрироваться</button>
+        </form>
+      </div>
+      </div>
+    );
+  }
 }
-export default Auth
+export default Auth;
--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -3,78 +3,92 @@ import styles from './QuizList.module.css';
 import QuizListItem from './QuizListItem/QuizListItem';
 
 class QuizList extends Component {
+
+  // state={
+  //   {
+  //     id:1,
+  //     label:
+
+  //   },
+  //   {},
+  //   {}
+  // }
   renderQuiz(){
-    return [1,2,3].map((quiz,index)=>{
-        return <QuizListItem quiz={quiz}  index={index}/>
-    })
+      return [1,2,3].map((quiz,index)=>{
+          return <QuizListItem quiz={quiz} key={index} index={index}/>
+      })}
 
-  }
   render() {
-    console.log(this.props)
-    // return (
-    //   <div className={styles.QuizList}>
-    //     <div>
-    //     <h1>Cписок тестов</h1>
-    //     {this.renderQuiz()}
-    //     </div>
-    //   </div>
-    // );
-    return(
-      <div className={styles.body}>
-    <div className={styles.container}>
-        <div className={styles.card}>
-            <div className={styles.face +' '+ styles.face1}>
-                <div className={styles.content}>
-                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true"/>
-                    <h3>Design</h3>
-                </div>
-            </div>
-            <div className={styles.face +' '+ styles.face2}>
-                <div className={styles.content}>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
-                        <a href="#">Go to test</a>
-                </div>
-            </div>
-        </div>
-       
-
-        <div className={styles.card}>
-            <div className={styles.face +' '+ styles.face1}>
-                <div className={styles.content}>
-                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true"/>
-                    <h3>Code</h3>
-                </div>
-            </div>
-            <div className={styles.face +' '+ styles.face2}>
-                <div className={styles.content}>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
-                        <a href="#">Go to test</a>
-                </div>
-            </div>
-        </div>
-      
-
-        <div className={styles.card}>
-            <div className={styles.face +' '+ styles.face1}>
-                <div className={styles.content}>
-                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true"/>
-                    <h3>Launch</h3>
-                </div>
-            </div>
-            <div className={styles.face +' '+ styles.face2}>
-                <div className={styles.content}>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
-                        <a href="#">Go to test</a>
-                </div>
-            </div>
-        </div>
-       
-</div>
-</div>
-
-   
-
-    )
+    console.log('innnn',this.props)
+    return (
+     
+       <div className={styles.QuizList}>
+         <h1> Список тестов</h1>
+         <div className={styles.body}>
+        <div className={styles.container}>
+        {this.renderQuiz()}
+       </div>
+       </div>
+     </div>
+     );
+     
+  
   }
 }
 export default QuizList;
+//     return(
+ // <div className={styles.body}>
+  //<div className={styles.container}>
+//       
+//         <div className={styles.card}>
+//             <div className={styles.face +' '+ styles.face1}>
+//                 <div className={styles.content}>
+//                     <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true"/>
+//                     <h3>Design</h3>
+//                 </div>
+//             </div>
+//             <div className={styles.face +' '+ styles.face2}>
+//                 <div className={styles.content}>
+//                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+//                         <a href="#">Go to test</a>
+//                 </div>
+//             </div>
+//         </div>
+       
+
+//         <div className={styles.card}>
+//             <div className={styles.face +' '+ styles.face1}>
+//                 <div className={styles.content}>
+//                     <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true"/>
+//                     <h3>Code</h3>
+//                 </div>
+//             </div>
+//             <div className={styles.face +' '+ styles.face2}>
+//                 <div className={styles.content}>
+//                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+//                         <a href="#">Go to test</a>
+//                 </div>
+//             </div>
+//         </div>
+      
+
+//         <div className={styles.card}>
+//             <div className={styles.face +' '+ styles.face1}>
+//                 <div className={styles.content}>
+//                     <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true"/>
+//                     <h3>Launch</h3>
+//                 </div>
+//             </div>
+//             <div className={styles.face +' '+ styles.face2}>
+//                 <div className={styles.content}>
+//                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+//                         <a href="#">Go to test</a>
+//                 </div>
+//             </div>
+//         </div>
+       
+// </div>
+// </div>
+
+   
+
